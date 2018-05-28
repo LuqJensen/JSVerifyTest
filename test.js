640f1bb1c5a81ffb6dc1657c91ec3363e828a1ab
LuqJensen <lucas__jensen@hotmail.com>
2018-05-28 21:39:36 +0200

    corrections

--- test.js
+++ test.js
@@ -33,21 +33,36 @@ var absIsNotAlways0 = jsc.checkForall(jsc.integer, (a) => a < 0 ? a + math.abs(a
 // we can determine whether the implementation of mod always calculates a result with the same sign as the 
 // dividend or divisor, ie. uses truncation(fixation) or floor function. 
 var modIsDonaldKnuth = jsc.checkForall(jsc.integer(-(Math.pow(2,31) -1), 0), (a) => math.mod(a, 2) >= 0);
-var specificationWorks = jsc.checkForall(jsc.integer(-(Math.pow(2,31) -1), 0), (a) => a - 2 * Math.floor(a / 2) >= 0);
-var modImplementationHonorsSpecification = jsc.checkForall(jsc.integer, jsc.integer, (a, b) => b === 0 || math.mod(a, b) === a - b * Math.floor(a / b));
 
 // Oops! .mod does not support negative divisors, according to docs: http://mathjs.org/docs/reference/functions/mod.html
 // this module is implemented as "x - y * floor(x / y)", but jsverify finds a counterexample where "a = -1"
 // according to WolframAlpha the stated expression equals "0" - http://www.wolframalpha.com/input/?i=(-1)+-+(-1)+*+floor((-1)+%2F+(-1))
+var modIsNotDivision = jsc.checkForall(jsc.integer, (a) => a === 0 || math.mod(a, a) === 0);
+// Lets check if the specification works...
+var specificationWorks1 = jsc.checkForall(jsc.integer(-(Math.pow(2,31) -1), 0), (a) => a - 2 * Math.floor(a / 2) >= 0);
+var specificationWorks2 = jsc.checkForall(jsc.integer, (a) => a === 0 || a - a * Math.floor(a / a) === 0);
 // We are unable to determine whether the implementation is Donald Knuth or Raymond T. Boute's Euclidean definition.
 // But one thing is certain, the implementation is lousy as it does not satisfy the stated specification.
-var modIsNotDivision = jsc.checkForall(jsc.integer, (a) => a === 0 || math.mod(a, a) === 0);
-var mod = jsc.checkForall(jsc.integer, (a) => a === 0 || a - a * Math.floor(a / a) === 0);
+var modImplementationHonorsSpecification = jsc.checkForall(jsc.integer, jsc.integer, (a, b) => b === 0 || math.mod(a, b) === a - b * Math.floor(a / b));
 
 // In the docs it is stated that the floor function is used for the mod function,
 // which could indicate a bad implementation of the floor function. 
 // That does not appear to be the case.
 var floorIsNotTheCulprit = jsc.checkForall(jsc.number, (a) => math.floor(a) <= a && a - math.floor(a) < 1);
+
+var fixIsAlwaysCloserToZero = jsc.checkForall(jsc.number, (a) => {
+    if (math.equal(a, 0)) 
+        return true;
+    
+    let result = math.fix(a);
+
+    if (!(a - result < 1)) 
+        return false;
+    if (a > 0) 
+        return math.floor(a) === result;
+    else 
+        return math.ceil(a) === result;
+});
 
 var sortIsConsistent = jsc.checkForall(jsc.integer(1, 98), (length) => {
     let matrix = Array.from({length: length}, () => jsc.random(-(Math.pow(2,31) - 1), Math.pow(2,31)- 1));
@@ -60,20 +75,22 @@ var sortIsConsistent = jsc.checkForall(jsc.integer(1, 98), (length) => {
 // Util
 //TODO: figure out test for math.clone(x)
  
-var isInteger = jsc.checkForall(jsc.integer, (a) => math.isInteger(a) == true);
+var isInteger = jsc.checkForall(jsc.integer, (a) => math.isInteger(a));
 
-var isNaN = jsc.checkForall(jsc.number, (a) => math.isNaN(a) == false);
+var isNaN = jsc.checkForall(jsc.number, (a) => !math.isNaN(a));
 
-var isNegative = jsc.checkForall(jsc.integer(-100,-1), (a) => math.isNegative(a) === a<0);
+var isNegative = jsc.checkForall(jsc.integer, (a) => math.isNegative(a) === a < 0);
 
 //Apparently 0 is not numeric.
-var isNumeric = jsc.checkForall(jsc.number, (a) => math.isNumeric == true);
+var isNumeric = jsc.checkForall(jsc.number, (a) => math.isNumeric(a));
 
 //0 is not positive pr. definition
-var isPositive = jsc.checkForall(jsc.integer(1,99), (a) => math.isPositive(a) === a>0);
+var isPositive = jsc.checkForall(jsc.integer, (a) => math.isPositive(a) === a > 0);
 
-var isZero = jsc.checkForall(jsc.integer(), (a) => math.isZero(a) === (a==0)); 
+var isZero = jsc.checkForall(jsc.integer, (a) => math.isZero(a) === (a === 0)); 
 
 
 console.log({sqrtIsReversible, additionIsCommutative, adding1TwiceEquals2Once, additionIsAssociative, multiplicationIsDistributive, absIsPositive, absIsNotAlways0,
-   modIsDonaldKnuth, specificationWorks, modImplementationHonorsSpecification, modIsNotDivision, mod, floorIsNotTheCulprit, sortIsConsistent, isInteger, isNaN, isPositive, isNegative, isNumeric, isZero});
+   modIsDonaldKnuth, modIsNotDivision, specificationWorks1, specificationWorks2, modImplementationHonorsSpecification, floorIsNotTheCulprit,
+   fixIsAlwaysCloserToZero,
+   sortIsConsistent, isInteger, isNaN, isPositive, isNegative, isNumeric, isZero});
